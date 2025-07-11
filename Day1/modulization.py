60c4aed65005c4f366500fb8f6c6ff64df29d0d0
Harsha H <harsha2041889@gmail.com>
2025-07-11 11:02:46 +0530

    Updated the Day1 Programs

---
+++ Day1/modulization.py
@@ -0,0 +1,14 @@
+ 
+def add(x:int,y:int):
+    """
+    This is descriptive comments
+    """
+    return x+y
+
+total_sum=100
+bonus_points=200
+final_score = add(total_sum,bonus_points)
+
+print(final_score)
+print(f"{type(final_score)}")
+
