d68793af1eca79e2e22b8fb22b6efdaf5a9d8440
Harsha H <harsha2041889@gmail.com>
2025-07-11 11:14:10 +0530

    Deleted day one folder from repo

--- Day1/addTwoNumber.py
+++
@@ -1,5 +0,0 @@
-# adding the two numbers
-x=100
-y =200
-z = x+y
-print(f"The result of adding {x} and {y} is {z}")
--- Day1/areaOfCircle.py
+++
@@ -1,6 +0,0 @@
-import math
-def getArea(radius:int):
-    return math.pi*radius**2
-
-radius = int(input("Enter the radius of the circle "))
-print(f"The area of the circle is {getArea(radius)}")
