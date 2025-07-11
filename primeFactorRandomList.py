5509202f91e033f1bcce1d1c9f8af5f245c1e349
Harsha H <harsha2041889@gmail.com>
2025-07-11 12:54:00 +0530

    Updated the Day2

---
+++ primeFactorRandomList.py
@@ -0,0 +1,6 @@
+
+lower_limit = input("Enter the lower limit>1: ")
+lower = (lower_limit.isdigit() and int(lower_limit) or None)
+
+upper_limit = input("Enter the upper limit: ")
+upper =(upper_limit.isdigit() and int(lower_limit) or None)
